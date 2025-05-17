8509dbe1c73b3777549a12a2b275f7a763cb7982
Amr230102611 <idkmora@gmail.com>
2025-05-17 23:53:03 +0300

    added cryptography page

--- WebSec230102611/resources/views/layouts/menu.blade.php
+++ WebSec230102611/resources/views/layouts/menu.blade.php
@@ -16,7 +16,12 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route('products_list')}}">Products</a>
             </li>
-            
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('cryptography')}}">Cryptography</a>
+            </li>
+            {{-- <li class="nav-item">
+                <a class="nav-link" href="{{route('webcrypto')}}">Web Crypto</a>
+            </li> --}}
             @if(auth()->check() && auth()->user()->can('show_users'))
             <li class="nav-item">
                 <a class="nav-link" href="{{route('users')}}">Users</a>
